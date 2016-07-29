f847ff8e2569a3e0d4c1744e7e9f23ffbd9bd79b
Chris Smalley <chrism.smalley@gmail.com>
2016-07-29 13:22:45 -0500

    Fixed Javascript Tree Quiz per Mat's request

--- quiz.js
+++ quiz.js
@@ -1,14 +1,9 @@
 var submitButton = document.getElementById("button");
 var heightInput = document.getElementById("height");
 var characterInput = document.getElementById("characters");
-var charHeight = heightInput && characterInput;
 
-var grow = function() {
-  var tree = {
-    height: document.getElementById("height").value ,
-    character: document.getElementById("characters").value
-  }
-  console.log(tree.height, tree.character);
+
+function grow (tree) {
 
   if (tree.height === "" || tree.character === "") {
     alert("Both fields must have a value");
@@ -16,19 +11,35 @@ var grow = function() {
   for (var i = 0; i < tree.height; i++) {
     var treeShape = " ".repeat(tree.height - i);
     treeShape += tree.character.repeat((2 * i) + 1);
-    document.getElementById("result").innerHTML += treeShape + "\n";
     console.log(treeShape);
-
   };
-};
+}
+
+function display(event) {
+  console.log(event);
+  event.preventDefault();
+    var tree = {
+      height: document.getElementById("height").value,
+      character: document.getElementById("characters").value
+  };
+  console.log(tree);
+  grow(tree);
+
+}
 
 // EVENT LISTENERS
-submitButton.addEventListener("click", grow);
+submitButton.addEventListener("click", display);
 
-charHeight.addEventListener("keydown", function(){
+characterInput.addEventListener("keydown", function(){
 
   if (event.keyCode === 13){
     submitButton.click();
   }
-});
+})
 
+heightInput.addEventListener("keydown", function(){
+
+  if (event.keyCode === 13){
+    submitButton.click();
+  }
+})
